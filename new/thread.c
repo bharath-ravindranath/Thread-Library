b32c77c021fb14c8496842cc5b39c8aa11a09afe
bharat3 <bharat3@ncsu.edu>
2016-01-25 00:32:28 -0500

    Init Create and Yield

--- new/thread.c
+++ new/thread.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ucontext.h>
+//#include "mythread.h"
 
 #define STACK_SIZE 8192
 
@@ -10,7 +11,11 @@ struct thread_node{
 	struct thread_node *parent;
 };
 struct thread_node *ready_queue_head = NULL;
+struct thread_node *running_thread;
+struct thread_node *temp;
 ucontext_t unix_context;
+typedef void *MyThread;
+
 
 void fifo_scheduler(){
 
@@ -19,9 +24,12 @@ void fifo_scheduler(){
 		temp = ready_queue_head;
 		ready_queue_head = ready_queue_head->next;
 		temp->next = NULL;
+		running_thread = temp;
 		swapcontext(&unix_context,&(temp->context));
 	}
 }
+
+
 
 void MyThreadInit(void(*start_funct)(void*), void *args){
 
@@ -40,14 +48,61 @@ void MyThreadInit(void(*start_funct)(void*), void *args){
 	fifo_scheduler();
 }
 
-void test_func(void *x){
+
+MyThread MyThreadCreate (void (*start_funct)(void *), void *args){
+
+	struct thread_node *temp1;
+	temp = malloc(sizeof(struct thread_node));
+	getcontext(&(temp->context));
+	temp->parent 					= running_thread;
+	temp->context.uc_link			= &unix_context;
+	temp->context.uc_stack.ss_sp	= malloc(STACK_SIZE);
+	temp->context.uc_stack.ss_size	= STACK_SIZE;
+	temp->next = NULL;
+	makecontext(&(temp->context), (void (*)(void))start_funct, 1, args);
 	
+	if(ready_queue_head == NULL) ready_queue_head = temp;
+
+	else {
+		temp1 = ready_queue_head;
+		while(temp1->next != NULL) temp1 = temp1->next;
+		temp1->next = temp;
+	}
+
+	return (MyThread)temp;
+
+}
+
+void MyThreadYield(void){
+
+	struct thread_node *temp1;
+	temp1 = ready_queue_head;
+	while(temp1->next != NULL) temp1 = temp1->next;
+	temp1->next = running_thread;
+	swapcontext(&(running_thread->context),&unix_context);
+}
+
+void test_func3(void *x){
+	printf("In funtion 3\n");
+}
+void test_func2(void *x){
 	//MyThread thread1;
 	int a=10;
+	printf("In test funtion 2\n");
+	
+	MyThreadYield();
+	printf("after the test funtion 2\n");
+}
+
+void test_func(void *x){
+	
+	MyThread thread1,thread2;
+	int a=10;
 	printf("In test funtion\n");
-	//thread1 = MyThreadCreate(test_func2,(void *)&a);
-	//MyThreadJoin(thread1);
-	//printf("after the test funtion\n");
+	thread1 = MyThreadCreate(test_func2,(void *)&a);
+	thread2 = MyThreadCreate(test_func3,(void *)&a);
+	MyThreadYield();
+	printf("after the test funtion\n");
 
 }
 
