19b878e3c4a8bbe5d6537a26efbbae85e804f3eb
bharat3 <bharat3@ncsu.edu>
2016-01-30 11:59:01 -0500

    before correcting fib

--- new/thread.c
+++ new/thread.c
@@ -9,6 +9,9 @@
 #define WAIT_ON_JOIN 1
 #define WAIT_ON_JOIN_ALL 2
 
+#define TH_ALIVE 1
+#define TH_FINISHED 2
+
 struct thread_node{
 	struct thread_node *next;
 	ucontext_t context;
@@ -16,11 +19,48 @@ struct thread_node{
 	int wait_status;
 	struct thread_node *waiting_on_thread;
 };
+
+struct list_of_threads{
+	struct thread_node *node;
+	int status;
+	struct list_of_threads *next;
+};
+
+struct list_of_threads *list = NULL;
+
 struct thread_node *ready_queue_head = NULL, *blocked_queue_head = NULL;
 struct thread_node *running_thread;
 struct thread_node *temp;
-ucontext_t unix_context;
+struct thread_node *unix_context;
 
+void add_to_list(struct thread_node *node){
+	struct list_of_threads *list_temp = malloc(sizeof(struct list_of_threads));
+	struct list_of_threads *list_temp1;
+	list_temp->node = node;
+	list_temp->next = NULL;
+	list_temp->status = TH_ALIVE;
+	if(list == NULL) list = list_temp;
+	else{
+		list_temp1 = list;
+		while(list_temp1->next != NULL) list_temp1 = list_temp1->next;
+		list_temp1->next = list_temp;
+	}
+
+}
+
+void kill_thread(struct thread_node *node){
+	struct list_of_threads *list_temp1 = list;
+	while(list_temp1->node != node) list_temp1 = list_temp1->next;
+	list_temp1->status = TH_FINISHED;
+}
+
+int is_terminated(struct thread_node *node){
+	struct list_of_threads *list_temp1 = list;
+	while(list_temp1->node != node) list_temp1 = list_temp1->next;
+	if(list_temp1->status == TH_FINISHED) return 1;
+	return 0;
+
+}
 
 void fifo_scheduler(){
 
@@ -29,7 +69,7 @@ void fifo_scheduler(){
 		running_thread = ready_queue_head;
 		ready_queue_head = ready_queue_head->next;
 		running_thread->next = NULL;
-		swapcontext(&unix_context,&(running_thread->context));
+		swapcontext(&unix_context->context,&(running_thread->context));
 		if(running_thread != NULL) printf("you have to exit the thread\n");
 
 	}
@@ -39,13 +79,18 @@ void MyThreadInit(void(*start_funct)(void*), void *args){
 
 
 	ready_queue_head 					= malloc(sizeof(struct thread_node));
+	unix_context						= malloc(sizeof(struct thread_node));
 	ready_queue_head->next 				= NULL;
-	ready_queue_head->parent  			= NULL;
+	unix_context->next					= NULL;
+	ready_queue_head->parent  			= unix_context;
+	unix_context->parent				= NULL;
 	ready_queue_head->wait_status		= NOT_WAITING;
+	unix_context->wait_status			= NOT_WAITING;
 	ready_queue_head->waiting_on_thread = NULL;
-
+	unix_context->waiting_on_thread		= NULL;
+	add_to_list(ready_queue_head);
 	getcontext(&(ready_queue_head->context));
-	ready_queue_head->context.uc_link			= &unix_context;
+	ready_queue_head->context.uc_link			= &unix_context->context;
 	ready_queue_head->context.uc_stack.ss_sp	= malloc(STACK_SIZE);
 	ready_queue_head->context.uc_stack.ss_size	= STACK_SIZE;
 
@@ -63,7 +108,7 @@ MyThread MyThreadCreate (void (*start_funct)(void *), void *args){
 	temp->waiting_on_thread	= NULL;
 
 	getcontext(&(temp->context));
-	temp->context.uc_link			= &unix_context;
+	temp->context.uc_link			= &unix_context->context;
 	temp->context.uc_stack.ss_sp	= malloc(STACK_SIZE);
 	temp->context.uc_stack.ss_size	= STACK_SIZE;
 	
@@ -76,7 +121,7 @@ MyThread MyThreadCreate (void (*start_funct)(void *), void *args){
 		while(temp1->next != NULL) temp1 = temp1->next;
 		temp1->next = temp;
 	}
-
+	add_to_list(temp);
 	return (MyThread)temp;
 }
 
@@ -90,7 +135,7 @@ void MyThreadYield(void){
 	
 	temp1->next 	= running_thread;
 	running_thread 	= NULL;
-	swapcontext(&(temp1->next->context),&unix_context);
+	swapcontext(&(temp1->next->context),&unix_context->context);
 }
 
 void MyThreadExit(void){
@@ -98,7 +143,7 @@ void MyThreadExit(void){
 	//swapcontext(&to_destroy,unix_context);
 	struct thread_node *temp1,*bl_temp1,*bl_temp2;
 	ucontext_t temp2 = running_thread->context;
-	 if (running_thread->parent != NULL && (running_thread->parent->wait_status == WAIT_ON_JOIN) && ((running_thread->parent)->waiting_on_thread == running_thread)){
+	 if (((running_thread->parent)->wait_status == WAIT_ON_JOIN) && ((running_thread->parent)->waiting_on_thread == running_thread)){
 		(running_thread->parent)->wait_status = NOT_WAITING;
 		bl_temp1 = blocked_queue_head;
 		bl_temp2 = blocked_queue_head;
@@ -109,6 +154,7 @@ void MyThreadExit(void){
 		if(bl_temp2 == bl_temp1) blocked_queue_head = NULL;
 		else{
 			bl_temp2->next = bl_temp1->next;
+			bl_temp1->next = NULL;
 		}
 
 		if(ready_queue_head == NULL) ready_queue_head = running_thread->parent;
@@ -118,7 +164,7 @@ void MyThreadExit(void){
 			temp1->next = running_thread->parent;
 		}
 	}
-	else if( running_thread->parent != NULL && running_thread->parent->wait_status == WAIT_ON_JOIN_ALL){
+	else if( running_thread->parent->wait_status == WAIT_ON_JOIN_ALL){
 		temp1 = ready_queue_head;
 		while(temp1 != NULL && temp1->parent != running_thread->parent ) temp1 = temp1->next;
 		if(temp1 == NULL){
@@ -137,7 +183,7 @@ void MyThreadExit(void){
 	}
 	free(running_thread);
 	running_thread = NULL;
-	swapcontext(&temp2,&unix_context);
+	swapcontext(&temp2,&unix_context->context);
 }
 
 int MyThreadJoin(MyThread thread){
@@ -169,7 +215,7 @@ int MyThreadJoin(MyThread thread){
 		temp1->waiting_on_thread 	= temp;
 		temp1->next = NULL;
 		running_thread = NULL;
-		swapcontext(&(temp1->context),&unix_context);
+		swapcontext(&(temp1->context),&unix_context->context);
 		return 0;
 	}
 	return -1;
@@ -192,40 +238,60 @@ void MyThreadJoinAll(void){
 			while(temp2->next != NULL) temp2 = temp2->next;
 			temp2->next = running_thread;
 			running_thread = NULL;
-			swapcontext(&(temp2->next->context), &unix_context);
+			swapcontext(&(temp2->next->context), &unix_context->context);
 		}
 	}
 }
-int mode = 0;
 
-void t0(void * n)
+
+
+
+void fib(void *in)
 {
-  MyThread T;
+	MyThread  T1,T2;
+  int *n = (int *)in;	 	/* cast input parameter to an int * */
 
-  int n1 = (int)n; 
-  printf("t0 start %d\n", n1);
+  if (*n == 0)
+    /* pass */;			/* return 0; it already is zero */
 
-  int n2 = n1 -1 ;
-  if (n1 > 0) {
-    printf("t0 create\n");
-    T = MyThreadCreate(t0, (void *)n2);
-    if (mode == 1)
-      MyThreadYield();
-    else if (mode == 2)
-      MyThreadJoin(T);
+  else if (*n == 1)
+    /* pass */;			/* return 1; it already is one */
+
+  else {
+    int n1 = *n - 1;		/* child 1 param */
+    int n2 = *n - 2;		/* child 2 param */
+
+    // create children; parameter points to int that is initialized.
+    // this is the location they will write to as well.
+    T1 = MyThreadCreate(fib, (void*)&n1);
+    T2 = MyThreadCreate(fib, (void*)&n2);
+    // after creating children, wait for them to finish
+    MyThreadJoin(T1);
+    MyThreadJoin(T2);
+    //MyThreadJoinAll();
+    //  write to addr n_ptr points; return results in addr pointed to
+    //  by input parameter
+    *n = n1 + n2;
   }
-  printf("t0 end\n");
-  MyThreadExit();
+
+  MyThreadExit();		// always call this at end
 }
 
-int main(int argc, char *argv[])
+main(int argc, char *argv[])
 {
-  int count; 
-  
-  if (argc < 2 || argc > 3)
-    return -1;
-  count = atoi(argv[1]);
-  if (argc == 3)
-    mode = atoi(argv[2]);
-  MyThreadInit(t0, (void *)count);
+  int n;
+
+  if (argc != 2) {
+    printf("usage: %s <n>\n", argv[0]);
+    exit(-1);
+  }
+  n = atoi(argv[1]);
+  if (n < 0 || n > 10) {
+    printf("invalid value for n (%d)\n", n);
+    exit(-1);
+  }
+
+  printf("fib(%d) = ", n);
+  MyThreadInit(fib, (void *)&n);
+  printf("%d\n", n);
 }
